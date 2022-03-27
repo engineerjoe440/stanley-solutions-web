e98852256b51ebac98734462c89b5368f66d4cf6
engineerjoe440 <engineerjoe440@yahoo.com>
2022-03-27 13:20:09 -0700

    set up new error pages

--- main.py
+++ main.py
@@ -8,14 +8,10 @@ Stanley Soutions Web
 ################################################################################
 
 # Imports
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-
-# Define the Generic Page
-GENERIC_HTML = """"""
 
 
 # Stand Up the App
@@ -26,30 +22,54 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 TEMPLATES = Jinja2Templates(directory="templates")
 
 # Index Page
-@app.get("/")
-async def root():
-    return HTMLResponse(
-        content=TEMPLATES.TemplateResponse(
-            "dog.html",
-            {
-                "CODE": "503",
-            },
-        ),
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return await get_404(request)
+
+@app.get("/404", response_class=HTMLResponse)
+async def get_404(request: Request):
+    return TEMPLATES.TemplateResponse(
+        "dog.html",
+        {
+            "request": request,
+            "CODE": "404",
+        },
+        status_code=404,
+    )
+
+@app.get("/503", response_class=HTMLResponse)
+async def get_503(request: Request):
+    return TEMPLATES.TemplateResponse(
+        "skull.html",
+        {
+            "request": request,
+            "CODE": "503",
+        },
         status_code=503,
     )
 
+@app.get("/418", response_class=HTMLResponse)
+async def get_418(request: Request):
+    return TEMPLATES.TemplateResponse(
+        "coffee.html",
+        {
+            "request": request,
+            "CODE": "418",
+        },
+        status_code=418,
+    )
+
 # Custom Page
-@app.get("/err/{error_code}")
-async def error_page(error_code: str):
+@app.get("/err/{error_code}", response_class=HTMLResponse)
+async def error_page(request: Request, error_code: str):
     if not (error_code.isdigit() and len(error_code) == 3):
         await root()
-    return HTMLResponse(
-        content=TEMPLATES.TemplateResponse(
-            "dog.html",
-            {
-                "CODE": error_code,
-            },
-        ),
+    return TEMPLATES.TemplateResponse(
+        "skull.html",
+        {
+            "request": request,
+            "CODE": error_code,
+        },
         status_code=int(error_code),
     )
 
